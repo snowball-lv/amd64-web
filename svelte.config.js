e81479e276f1f567085f2d9fd96e5e6c531f4db0
snowball <snowball@inbox.lv>
2023-06-10 12:27:20 +0300

    rebuild

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +14,8 @@ const config = {
             precompress: false,
             strict: true
 		})
-	}
+	},
+	preprocess: preprocess()
 };
 
 export default config;
